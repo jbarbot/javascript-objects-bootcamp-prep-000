5e3bda83825e334064bc41804676d0ae9fede303
Jacob Barbot <barbotjacob@gmail.com>
2018-06-27 02:51:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['My Bloody Valentine'] = 'Black Balloon'
   playlist['Phil Ochs'] = 'Song Name1';
   playlist['Slowdive'] = 'Not sure what they sing';
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistname) {
